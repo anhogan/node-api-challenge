7d18ed90aa6bf59db7d0aa16a33e44ed6e1b18c6
Amanda Hogan <ahogan@teamworks.com>
2020-03-13 11:48:34 -0400

    Setup project routes

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -7,31 +7,116 @@ const Actions = require('../data/helpers/actionModel');
 const router = express.Router();
 
 router.post('/', validateProject, (req, res) => {
+  const data = { ...req.body, completed: false };
 
+  Projects.insert(data)
+    .then(project => {
+      console.log(`Successfully created ${project}`);
+      Projects.get()
+        .then(projects => {
+          res.status(201).json(projects);
+        })
+        .catch(error => {
+          console.log(error);
+          res.status(500).json({ message: "The project data could not be retrieved" });
+        });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "The project could not be created" });
+    });
 });
 
 router.post('/:id/actions', validateProjectId, validateAction, validateLength, (req, res) => {
+  const data = { ...req.body, project_id: req.params.id, completed: false };
 
+  Actions.insert(data)
+    .then(action => {
+      console.log(` Successfully created ${action}`);
+      Projects.getProjectActions(req.params.id)
+        .then(actions => {
+          res.status(201).json(actions);
+        })
+        .catch(error => {
+          console.log(error);
+          res.status(500).json({ message: "The action data could not be retrieved" });
+        });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "The action could not be created" });
+    });
 });
 
 router.get('/', (req, res) => {
-
+  Projects.get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "The project data could not be retrieved" });
+    });
 });
 
 router.get('/:id', validateProjectId, (req, res) => {
-
+  Projects.get(req.params.id)
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "The project data could not be retrieved" });
+    });
 });
 
 router.get('/:id/actions', validateProjectId, (req, res) => {
-
+  Projects.getProjectActions(req.params.id)
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "The action data could not be retrieved" });
+    });
 });
 
 router.delete('/:id', validateProjectId, (req, res) => {
-
+  Projects.remove(req.params.id)
+    .then(project => {
+      console.log(`Successfully deleted ${project}`);
+      Projects.get()
+        .then(projects => {
+          res.status(200).json(projects);
+        })
+        .catch(error => {
+          console.log(error);
+          res.status(500).json({ message: "The project data could not be retrieved" });
+        });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "The project could not be deleted" })
+    })
 });
 
 router.put('/:id', validateProjectId, validateProject, (req, res) => {
-
+  Projects.update(req.params.id, req.body)
+    .then(project => {
+      console.log(`Successfully updated ${project}`);
+      Projects.get()
+        .then(projects => {
+          res.status(200).json(projects);
+        })
+        .catch(error => {
+          console.log(error);
+          res.status(500).json({ message: "The project data could not be retrieved" });
+        });
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ message: "The project could not be updated" })
+    })
 });
 
 function validateProjectId(req, res, next) {
